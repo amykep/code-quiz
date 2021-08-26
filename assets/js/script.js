26bbd887e3872f72c7cf6686c54f9bd6ccf9ebda
AK <keppleramy@gmail.com>
2021-08-25 22:28:05 -0700

    questions-added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,94 @@
 // declare variables
-let wrapperEl = document.querySelector("wrapper");
-let timerEl = document.querySelector("#timer");
+var wrapperEl = document.querySelector("wrapper");
+var timerEl = document.querySelector("#timer");
 const codeQuestionsEl = document.querySelector("#quizquestions");
-let qchoicesEl = document.querySelector("#qchoices");
-let startQuizEl = document.querySelector("#startquiz");
+var qchoicesEl = document.querySelector("#qchoices");
+var startQuizEl = document.querySelector("#startquiz");
 
-let secondsLeft = 35;
-let timeInterval = 0;
-let penalty = 10;
+var secondsLeft = 35;
+var timeInterval = 0;
+var penalty = 10;
+var choicesList = document.createElement("ul");
+var questionIndex = 0
 
-// Triggers timer on button, shows user a display of questions on the screen
-startQuizEl.addEventListener("click", function ()
-{
-    timeInterval = setInterval(function ()
+
+var questions = [
     {
-        secondsLeft--;
-        timerEl.textContent = "Time: " + secondsLeft;
-
-        if (secondsLeft <= 0)
-        {
-            clearInterval(timeInterval);
-            timerEl.textContent = "Time's up!";
-        }
-    }, 1000);
-
-    // }
-    // render(questionIndex);
-});
-
-let questions = [
-    {
-        title: 'Commonly used data types DO NOT include:',
-        choices: ['strings', 'booleans', 'alerts', 'numbers'],
-        answer: 'alerts'
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
     },
     {
-        title: 'The condition in an if / else statement is enclosed within ____.',
-        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-        answer: 'parenthese'
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
     },
     {
-        title: 'Arrays in JavaScript can be used to store ____.',
-        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-        answer: 'all of the above'
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
     },
     {
-        title: 'String values must be enclosed within ____ when being assigned to variables.',
-        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-        answer: 'quotes'
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
     },
     {
-        title: 'A very useful tool used during development and debugging for printing content to the debugger is',
-        choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
-        answer: 'console.log'
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
     }
 ];
 
 
-
-
-
-// append quiz questions
-let questionIndex = function ()
+// Triggers timer on button, shows user a display of questions on the screen
+startQuizEl.addEventListener("click", function ()
 {
-    for (i = 0; i < questions.length; i++)
+    console.log("timer")
+    if (timeInterval === 0)
     {
-        let questionTitle = questions[questionIndex].title;
-        let questionChoices = questions[questionIndex].choices;
-
-        // append quiz titles
-        codeQuestionsEl.textContent = questionTitle;
-
-        // append quiz choices
-        questionChoices.forEach(function ()
+        timeInterval = setInterval(function ()
         {
-            let choicesList = document.createElement("ul");
-            let listItem = document.createElement("li");
-            listItem.textContent = newItem;
-            listItem.className = "li";
-            codeQuestionsEl.appendChild(choicesList);
-            choicesList.appendChild(listItem);
-            listItem.addEventListener("click", (compare));
-        }
-        )
+            secondsLeft--;
+            timerEl.textContent = "Time: " + secondsLeft;
 
-        // compare choices with answer
-        for (let i = o; i < questions.length; i++)
-        {
-            let questionTitleEl = document.createElement("questions.value")
-
-        }
+            if (secondsLeft <= 0)
+            {
+                clearInterval(timeInterval);
+                timerEl.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+    if ((questionIndex + 1) < questions.length)
+    {
+        questionIndex++;
     }
 
-    // create new li for every question choice
-    let choicesLiEl = document.createElement("li");
-    choicesLiEl.className = "li";
-    codeQuestions.appendChild(choicesLiEl);
 }
+);
+
+function processQuestion(newChoice)
+{
+    var listItem = document.createElement("li");
+    listItem.textContent = newChoice;
+    listItem.className = "li";
+    codeQuestionsEl.appendChild(choicesList);
+    choicesList.appendChild(listItem);
+}
+
+// append quiz questionstimer
+function render(param)
+{
+    codeQuestionsEl.innerHTML = "";
+    choicesList.innerHTML = "";
+
+    var questionTitle = questions[param].title;
+    var questionChoices = questions[param].choices;
+    codeQuestionsEl.textContent = questionTitle;
+    // codeQuestionsEl.className = "li";
+
+    // append quiz choices
+    questionChoices.forEach(processQuestion)
+}
+
